3988224801382d367ebfb9a095f6611c3f019ec8
NiteshChauhan430 <50042119+NiteshChauhan430@users.noreply.github.com>
2020-03-31 19:43:21 +0530

    SpringBootCacheExampleBasic

---
+++ SpringBootCacheExample/SpringBootCacheExample/src/main/java/com/SpringBootCacheExample/SpringBootCacheExampleApplication.java
@@ -0,0 +1,14 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplications
+@EnableCaching
+public class SpringBootCacheExampleApplication 
+{
+public static void main(String[] args) 
+{
+	SpringApplication.run(SpringBootCacheExampleApplication.class, args);
+}
+}
---
+++ SpringBootCacheExample/SpringBootCacheExample/src/main/java/com/SpringBootCacheExample/controller/CustomerController.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.javatpoint.model.Customer;
+
+@RestController
+public class CustomerController 
+{
+
+@GetMapping("/customerinfo")
+@Cacheable(value="customerInfo")
+public List customerInformation()
+{
+System.out.println("customer information from cache");
+	List detail=Arrays.asList(new Customer(5126890,"Charlie Puth","Current A/c", 450000.00),
+			 				  new Customer(7620015,"Andrew Flintoff","Saving A/c", 210089.00)
+							 );
+return detail;
+}
+}
